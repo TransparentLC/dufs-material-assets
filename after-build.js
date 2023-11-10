f7746fdf35b3cdb14819697d8808d1262229148c
✨小透明・宸✨ <47057319+TransparentLC@users.noreply.github.com>
2023-11-10 12:18:50 +0800

    Update after-build.js

--- after-build.js
+++ after-build.js
@@ -1,7 +1,13 @@
 import fs from 'node:fs';
 
 let index = fs.readFileSync('dist/index.html', {encoding: 'utf-8'});
-index = index.replace(/<link href="\/(.*?)" rel="icon" type="image\/svg\+xml">/g, '<link href="__ASSETS_PREFIX__$1" rel="icon" type="image/svg+xml">');
-index = index.replace(/<link href="\/(.*?)" rel="stylesheet">/g, '<link href="__ASSETS_PREFIX__$1" rel="stylesheet">');
-index = index.replace(/<script crossorigin src="\/(.*?)" type="module">/g, '<script crossorigin src="__ASSETS_PREFIX__$1" type="module">');
+console.log(index);
+index = index.replace(/<link href="\/([^"]*?)" rel="stylesheet">/g, '<link href="__ASSETS_PREFIX__$1" rel="stylesheet">');
+console.log(index);
+index = index.replace(/<script crossorigin src="\/([^"]*?)" type="module">/g, '<script crossorigin src="__ASSETS_PREFIX__$1" type="module">');
+if (process.env.DUFS_EMBED_FILENAME) {
+    index = index.replace(/<link href="\/([^"]*?)" rel="icon">/g, '<link href="__ASSETS_PREFIX__$1" rel="icon">');
+} else {
+    index = index.replace(/<link href="\/([^"]*?)" rel="icon" type="image\/svg\+xml">/g, '<link href="__ASSETS_PREFIX__$1" rel="icon" type="image/svg+xml">');
+}
 fs.writeFileSync('dist/index.html', index);
